4cecc24b0ad0a4f0c507918cfba160031d8d58c2
john cotamo <johncotamo@hotmail.com>
2023-07-17 09:50:19 -0500

    se añade formulario

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
+
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal,setModal] = useState(false)
 
+
+  const handeNuevoHasto =() =>{
+setModal(true)
+  }
   return (
     <>
       <div>
-        <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto} isValidPresupuesto={isValidPresupuesto} setIsValidPresupuesto={setIsValidPresupuesto}/>
+        <Header
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          isValidPresupuesto={isValidPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+        {isValidPresupuesto && (
+          <div className="nuevo-gasto">
+            <img src={IconoNuevoGasto} alt="nuevo gasto" onClick={handeNuevoHasto} />
+
+          </div>
+        )}
+
+        {modal && <Modal
+        setModal={setModal}
+        />}
       </div>
     </>
   );
